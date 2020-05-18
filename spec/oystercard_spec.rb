3117943e5c14497e5a843007cf9e78645b9f94a4
Sumner1185 <alastair.sumner@yahoo.com>
2020-05-18 15:08:59 +0100

    Add initialize method and attr_reader for balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,9 @@
 require 'oystercard'
 describe Oystercard do
+  let(:card) { Oystercard.new }
+
+  it 'should initialize with a default balance of 0' do
+    expect(card.balance).to eq 0
+  end
  
 end
